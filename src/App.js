85ea5a60ef932688045470dc3d1ba89d3ea2e684
keunkkk <egim4634@gmail.com>
2024-03-08 18:13:54 +0900

    modified:   src/App.js 	new file:   src/components/ex01_jsx/MyJsx001.js 	new file:   src/components/ex01_jsx/MyJsx002.js 	new file:   src/components/ex01_jsx/MyJsx003.css 	new file:   src/components/ex01_jsx/MyJsx003.js 	new file:   src/components/ex01_jsx/MyJsx004.js 	new file:   src/components/ex02_useState/MyBasic001.js 	new file:   src/components/ex02_useState/MyUseState002.js 	new file:   src/components/ex02_useState/MyUseState003.js 	new file:   src/components/ex02_useState/MyUseState004.js 	new file:   src/components/ex02_useState/MyUseState005.js 	new file:   src/components/ex02_useState/MyUseState006.js 	new file:   src/components/ex03_useRef/MyUseRef001.js 	new file:   src/components/ex03_useRef/MyUseRef002.js 	new file:   src/components/ex04_useEffect/MyUseEffect001.js 	new file:   src/components/ex05_async/MyuseStateSync001.js 	new file:   src/components/ex05_async/MyuseStateSync002.js 	new file:   src/components/ex05_async/MyuseStateSync003.js 	new file:   src/components/ex06_props/Myprops001.js 	new file:   src/components/ex06_props/Myprops002.js 	new file:   src/components/ex07_context/Left01.js 	new file:   src/components/ex07_context/Left02.js 	new file:   src/components/ex07_context/Left03.js 	new file:   src/components/ex07_context/MyuseContext01.css 	new file:   src/components/ex07_context/MyuseContext01.js 	new file:   src/components/ex07_context/Right01.js 	new file:   src/components/ex07_context/Right02.js 	new file:   src/components/ex07_context/Right03.js 	new file:   src/components/ex07_context/contexts/ThemeContext.js 	new file:   src/components/ex07_context/contexts/UseContext.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import MyJsx001 from "./components/ex01_jsx/MyJsx001";
+import MyJsx002 from "./components/ex01_jsx/MyJsx002";
+import MyJsx003 from "./components/ex01_jsx/MyJsx003";
+import MyJsx004 from "./components/ex01_jsx/MyJsx004";
+import MyBasic001 from "./components/ex02_useState/MyBasic001";
+import MyUseState002 from "./components/ex02_useState/MyUseState002";
+import MyUseState003 from "./components/ex02_useState/MyUseState003";
+import MyUseState004 from "./components/ex02_useState/MyUseState004";
+import MyUseState005 from "./components/ex02_useState/MyUseState005";
+import MyUseState006 from "./components/ex02_useState/MyUseState006";
+import MyUseRef001 from "./components/ex03_useRef/MyUseRef001";
+import MyUseRef002 from "./components/ex03_useRef/MyUseRef002";
+import MyUseEffect001 from "./components/ex04_useEffect/MyUseEffect001";
+import MyuseStateSync001 from "./components/ex05_async/MyuseStateSync001";
+import MyuseStateSync002 from "./components/ex05_async/MyuseStateSync002";
+import MyuseStateSync003 from "./components/ex05_async/MyuseStateSync003";
+import Myprops001 from "./components/ex06_props/Myprops001";
+import Myprops002 from "./components/ex06_props/Myprops002";
+import MyuseContext01 from "./components/ex07_context/MyuseContext01";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <MyJsx001 /> */}
+      {/* <MyJsx002 /> */}
+      {/* <MyJsx003 /> */}
+      {/* <MyJsx004 /> */}
+      {/* <MyBasic001 /> */}
+      {/* <MyUseState002 /> */}
+      {/* <MyUseState003 /> */}
+      {/* <MyUseState004 /> */}
+      {/* <MyUseState005 /> */}
+      {/* <MyUseState006 /> */}
+      {/* <MyUseRef001 /> */}
+      {/* <MyUseRef002 /> */}
+      {/* <MyUseEffect001 /> */}
+      {/* <MyuseStateSync001 /> */}
+      {/* <MyuseStateSync002 /> */}
+      {/* <MyuseStateSync003 /> */}
+      {/* <Myprops001 name="홍길동" /> */}
+      {/* <Myprops002 name="홍길동" age="30" loc="서울" /> */}
+      <MyuseContext01 />
     </div>
   );
 }
